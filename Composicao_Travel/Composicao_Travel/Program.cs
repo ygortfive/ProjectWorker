dd2b27e796d9fc739591849b3b8d09df65b4bd0c
Ygor Mattos <ygormattos.b@gmail.com>
2019-03-25 20:49:47 -0300

    Project Travel

---
+++ Composicao_Travel/Composicao_Travel/Program.cs
@@ -0,0 +1,35 @@
+﻿using Composicao_Travel.Entities;
+using System;
+
+namespace Composicao_Travel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("Wow That's Awesome!");
+            Post p1 = new Post(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to New Zealand",
+                "I'm going to visit this wonderful country",
+                12);
+            p1.addComment(c1);
+            p1.addComment(c2);
+
+            Comment c3 = new Comment("Good Night");
+            Comment c4 = new Comment("May the Force be with you");
+            Post p2 = new Post(
+                DateTime.Now,
+                "Good night guys",
+                "See you tomorrow",
+                5);
+            p2.addComment(c3);
+            p2.addComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine();
+            Console.WriteLine(p2);
+        }
+    }
+}
